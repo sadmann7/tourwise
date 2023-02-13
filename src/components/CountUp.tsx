ccb03132db05e951300cefd72087571b957eb300
sadmann7 <sadmann898.gmail.com>
2023-02-14 03:30:57 +0600

    add margin

--- src/components/CountUp.tsx
+++ src/components/CountUp.tsx
@@ -30,7 +30,11 @@ const CountUp = ({
   }, [end, duration]);
 
   return (
-    <div className={className} style={{ transitionDelay: `${delay}ms` }}>
+    <div
+      aria-label="count up from 0 to actual number"
+      className={className}
+      style={{ transitionDelay: `${delay}ms` }}
+    >
       {count.toFixed(0)}
     </div>
   );
