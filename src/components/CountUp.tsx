dc5a95762737f40035dd9b552c94f90660756602
sadmann7 <sadmann898.gmail.com>
2023-02-14 03:23:20 +0600

    add CountUp component

---
+++ src/components/CountUp.tsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+
+type CountUpProps = {
+  end: number;
+  duration?: number;
+  delay?: number;
+  className?: string;
+};
+
+const CountUp = ({
+  end,
+  duration = 1000,
+  delay = 0,
+  className,
+}: CountUpProps) => {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCount((prev) => {
+        const next = prev + end / (duration / 10);
+        if (next >= end) {
+          clearInterval(interval);
+          return end;
+        }
+        return next;
+      });
+    }, 10);
+    return () => clearInterval(interval);
+  }, [end, duration]);
+
+  return (
+    <div className={className} style={{ transitionDelay: `${delay}ms` }}>
+      {count.toFixed(0)}
+    </div>
+  );
+};
+
+export default CountUp;
